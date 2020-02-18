12bf0e5f84736bfdbbefc31318f964391ad8c6ff
roveil <dvsroveil@gmail.com>
2020-02-18 23:12:19 +0500

    fix tests

--- spam_filter/tests/test_models.py
+++ spam_filter/tests/test_models.py
@@ -1,22 +1,20 @@
-from multiprocessing.pool import ThreadPool as Pool
-from django.db import DatabaseError
-from django.test import TransactionTestCase
+from django.db import IntegrityError
+from django.test.testcases import TestCase
 
+from django_ml_spam_filter.utils import exec_in_parallel
 from spam_filter.models import NNStructure
 
 
-class SingleRowTableTest(TransactionTestCase):
+class SingleRowTableTest(TestCase):
 
     def test_single_row_table_concurrency(self, proc_num: int = 10):
         def _create(*_):
             try:
                 NNStructure.objects.create(weights=b'test')
-            except DatabaseError:
+            except IntegrityError:
                 pass
 
-        with Pool(proc_num) as pool:
-            pool.map(_create, [i for i in range(proc_num)])
-            pool.close()
-            pool.join()
+        args = [((i,), {}) for i in range(proc_num)]
+        exec_in_parallel(_create, args, processes_count=proc_num, need_db_refresh=True)
 
         self.assertEqual(1, NNStructure.objects.nocache().count())
